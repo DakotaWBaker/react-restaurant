ae31f5a7f69fbef7c8e041528cd10712047f1d80
DakotaWBaker <dakota.baker@student.awesomeinc.org>
2022-11-11 02:58:48 +0000

    saving

--- app/src/Menu.js
+++ app/src/Menu.js
@@ -5,8 +5,10 @@ import Dinner from './Dinner';
 import Breakfast from './Breakfast';
 import Dessert from './Dessert';
 import dayjs from 'dayjs';
+import {useState, useEffect} from "react"
 let now = dayjs();
 let time = now.format("h:mm:ss a");
+
 console.log(time.toLocaleString())
 
 
@@ -15,11 +17,32 @@ console.log(time.toLocaleString())
 
 
 export default function Menu({ name, menuItems, menuType,props }) {
-    const apps = menuItems.filter((el) => el.category.title === "Appetizers").map(item => <Appetizers  item= {item} />);
-    const lunch= menuItems.filter((el) => el.category.title === "Lunch" ).map(item => <Lunch  item= {item} />);
-    const din= menuItems.filter((el) => el.category.title === "Dinner" ).map(item => <Dinner  item= {item} />);
-    const breakfast= menuItems.filter((el) => el.category.title === "Breakfast" ).map(item => <Breakfast  item= {item} />);
-    const dessert = menuItems.filter((el) => el.category.title === "Dessert" ).map(item => <Dessert  item= {item} />);
+    // console.log(menuItems[17].location[0].location)
+    // let filteredLocation = menuItems.filter((el) => el.location == 2)
+    // console.log(filteredLocation)
+    const [location, setLocation] = useState('broadway');
+    console.log(location)
+    let apps = ''
+    let lunch= ''
+    let din=  ''
+    let breakfast= ''
+    let dessert = ''
+    
+    // console.log(menuItems[17].location[0].location)
+    if (location === "broadway") {
+     apps = menuItems.filter((el) => el.category.title === "Appetizers" && el.location == 2).map(item => <Appetizers  item= {item} />);
+     lunch= menuItems.filter((el) => el.category.title === "Lunch" && el.location == 2 ).map(item => <Lunch  item= {item} />);
+     din= menuItems.filter((el) => el.category.title === "Dinner" && el.location == 2 ).map(item => <Dinner  item= {item} />);
+     breakfast= menuItems.filter((el) => el.category.title === "Breakfast" && el.location == 2 ).map(item => <Breakfast  item= {item} />);
+     dessert = menuItems.filter((el) => el.category.title === "Dessert" && el.location == 2 ).map(item => <Dessert  item= {item} />);
+  }
+  else {
+    apps = menuItems.filter((el) => el.category.title === "Appetizers" && el.location == 1).map(item => <Appetizers  item= {item} />);
+    lunch= menuItems.filter((el) => el.category.title === "Lunch" ).map(item => <Lunch  item= {item} />);
+    din= menuItems.filter((el) => el.category.title === "Dinner" ).map(item => <Dinner  item= {item} />);
+    breakfast= menuItems.filter((el) => el.category.title === "Breakfast" ).map(item => <Breakfast  item= {item} />);
+    dessert = menuItems.filter((el) => el.category.title === "Dessert" ).map(item => <Dessert  item= {item} />);
+  }
     console.log(menuItems);
  
     if (name== 'dinner') {
@@ -32,6 +55,28 @@ export default function Menu({ name, menuItems, menuType,props }) {
   <div className = "container menu">
   <div className = "row text-center header">
   <h1>Menu</h1>
+  <div className="dropdown mb-3">
+  <a
+    className="btn btn-secondary dropdown-toggle"
+    href="#"
+    role="button"
+    id="dropdownMenuLink"
+    data-toggle="dropdown"
+    aria-haspopup="true"
+    aria-expanded="false"
+  >
+    Select Location
+  </a>
+  <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <button className="dropdown-item" onClick={() => setLocation("main")}>
+      Main St
+    </button>
+    <button className="dropdown-item" onClick={() => setLocation("broadway")}>
+      S. Broadway
+    </button>
+  </div>
+</div>
+
   </div>
   <div className = "row text-center mb-5">
          <div className = "col"> <button className ="menus" id = "dinner" onClick={() =>  menuType('dinner')}>Dinner</button></div>
@@ -51,6 +96,7 @@ export default function Menu({ name, menuItems, menuType,props }) {
      <div className = "row">
       <div className = "col text-center">
         <h2>Dinner</h2> 
+  
         </div>
      </div>
       <div className = "row">
@@ -70,6 +116,27 @@ export default function Menu({ name, menuItems, menuType,props }) {
       <div className = "container menu">
       <div className = "row text-center header">
       <h1>Menu</h1>
+      <div className="dropdown mb-3">
+  <a
+    className="btn btn-secondary dropdown-toggle"
+    href="#"
+    role="button"
+    id="dropdownMenuLink"
+    data-toggle="dropdown"
+    aria-haspopup="true"
+    aria-expanded="false"
+  >
+    Select Location
+  </a>
+  <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <button className="dropdown-item" onClick={() => setLocation("main")}>
+      Main St
+    </button>
+    <button className="dropdown-item" onClick={() => setLocation("broadway")}>
+      S. Broadway
+    </button>
+  </div>
+</div>
       </div>
       <div className = "row text-center mb-5">
              <div className = "col"> <button className ="menus" id = "dinner" onClick={() =>  menuType('dinner')}>Dinner</button></div>
@@ -108,6 +175,27 @@ export default function Menu({ name, menuItems, menuType,props }) {
       <div className = "container menu">
       <div className = "row text-center header">
       <h1>Menu</h1>
+      <div className="dropdown mb-3">
+  <a
+    className="btn btn-secondary dropdown-toggle"
+    href="#"
+    role="button"
+    id="dropdownMenuLink"
+    data-toggle="dropdown"
+    aria-haspopup="true"
+    aria-expanded="false"
+  >
+    Select Location
+  </a>
+  <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <button className="dropdown-item" onClick={() => setLocation("main")}>
+      Main St
+    </button>
+    <button className="dropdown-item" onClick={() => setLocation("broadway")}>
+      S. Broadway
+    </button>
+  </div>
+</div>
       </div>
       <div className = "row text-center">
              <div className = "col"> <button className ="menus" id = "dinner" onClick={() =>  menuType('dinner')}>Dinner</button></div>
@@ -146,6 +234,27 @@ export default function Menu({ name, menuItems, menuType,props }) {
       <div className = "container menu">
       <div className = "row text-center header">
       <h1 className='menu' >Menu</h1>
+      <div className="dropdown mb-3">
+  <a
+    className="btn btn-secondary dropdown-toggle"
+    href="#"
+    role="button"
+    id="dropdownMenuLink"
+    data-toggle="dropdown"
+    aria-haspopup="true"
+    aria-expanded="false"
+  >
+    Select Location
+  </a>
+  <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <button className="dropdown-item" onClick={() => setLocation("main")}>
+      Main St
+    </button>
+    <button className="dropdown-item" onClick={() => setLocation("broadway")}>
+      S. Broadway
+    </button>
+  </div>
+</div>
       </div>
       <div className = "row text-center mb-5">
              <div className = "col"> <button className ="menus" id = "dinner" onClick={() =>  menuType('dinner')}>Dinner</button></div>
@@ -184,6 +293,27 @@ export default function Menu({ name, menuItems, menuType,props }) {
       <div className = "container menu">
       <div className = "row text-center header">
       <h1>Menu</h1>
+      <div className="dropdown mb-3">
+  <a
+    className="btn btn-secondary dropdown-toggle"
+    href="#"
+    role="button"
+    id="dropdownMenuLink"
+    data-toggle="dropdown"
+    aria-haspopup="true"
+    aria-expanded="false"
+  >
+    Select Location
+  </a>
+  <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <button className="dropdown-item" onClick={() => setLocation("main")}>
+      Main St
+    </button>
+    <button className="dropdown-item" onClick={() => setLocation("broadway")}>
+      S. Broadway
+    </button>
+  </div>
+</div>
       </div>
       <div className = "row text-center mb-5">
              <div className = "col"> <button className ="menus" id = "dinner" onClick={() =>  menuType('dinner')}>Dinner</button></div>
